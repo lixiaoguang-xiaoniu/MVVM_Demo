3b5811742f888eb837ee39b6fd0735a8fb3b06a2
lixiaoguang <lixiaoguang@xiaoniu66.com>
2016-08-04 22:12:06 +0800

    setup

---
+++ MVVM_Demo/MVVM_Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MVVM_Demo
+//
+//  Created by xiaoniu on 8/4/16.
+//  Copyright © 2016 xiaoniu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MVVM_Demo/MVVM_Demo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MVVM_Demo
+//
+//  Created by xiaoniu on 8/4/16.
+//  Copyright © 2016 xiaoniu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
