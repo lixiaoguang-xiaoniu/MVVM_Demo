8f4e7ef61a99c2bcf0aa82fa2163284bf3f51f27
lixiaoguang <lixiaoguang@xiaoniu66.com>
2016-08-24 07:30:42 +0800

    textfield text check & tableView study

--- MVVM_Demo/MVVM_Demo/LXGBusinessViewController.m
+++ MVVM_Demo/MVVM_Demo/LXGBusinessViewController.m
@@ -8,19 +8,102 @@
 
 #import "LXGBusinessViewController.h"
 
+@interface  LXGBusinessViewController()<UITableViewDataSource,UITableViewDelegate>
+{
+    NSIndexPath *indexPath;
+}
+/** table */
+/** test string */
+@property (nonatomic, strong) NSMutableString *myStr;
+/** dic */
+@property (nonatomic, strong) NSMutableDictionary<NSIndexPath*,NSNumber*> *testDict;
+
+@end
+
+
+
 @implementation LXGBusinessViewController
+
+-(NSMutableDictionary *)testDict{
+    if(!_testDict){
+        _testDict = [NSMutableDictionary dictionary];
+    }
+    return _testDict;
+}
+
 -(void)viewDidLoad{
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor greenColor];
-    UILabel *label = [[UILabel alloc]init];
-    label.backgroundColor = [UIColor greenColor];
-    [self.view addSubview:label];
-    label.layer.cornerRadius = 0.5;
-    
-    label.text = @"my label";
-    label.frame = CGRectMake(100, 100, 100, 100);
-    
-    label.clipsToBounds = YES;
+    UITableView *tableView = [[UITableView alloc]initWithFrame:self.view.bounds style:UITableViewStyleGrouped];
+    [self.view addSubview:tableView];
+//    UILabel *label = [[UILabel alloc]init];
+//    label.backgroundColor = [UIColor greenColor];
+//    [self.view addSubview:label];
+//    label.layer.cornerRadius = 0.5;
+//    _myStr = [NSMutableString stringWithString:@"my label"];
+//    label.text = _myStr;
+//    label.frame = CGRectMake(100, 100, 100, 100);
+//    
+//    label.clipsToBounds = YES;
+//    
+//    UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
+//    btn.backgroundColor = [UIColor grayColor];
+//    [btn setTitle:@"要和角色" forState:UIControlStateNormal];
+//    [btn setTitleColor:[UIColor redColor] forState:UIControlStateNormal];
+//    btn.frame = CGRectMake(100, 300, 100, 100);
+//    [self.view addSubview:btn];
+}
 
+#pragma makr - UITableViewDelegate
+
+-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+    return 20;
+}
+
+#pragma mark - UITableViewDataSource
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
+    return 5;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return 5;
+}
+
+
+
+// Row display. Implementers should *always* try to reuse cells by setting each cell's reuseIdentifier and querying for available reusable cells with dequeueReusableCellWithIdentifier:
+// Cell gets various attributes set automatically based on table (separators) and data source (accessory views, editing controls)
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    UITableViewCell *cell = [[UITableViewCell alloc]init];
+    cell.backgroundColor = [UIColor redColor];
+    return cell;
+}
+
+
+-(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+////    [_myStr deleteCharactersInRange:NSMakeRange(0, _myStr.length)];
+//    static NSInteger myNum = 0;
+//    static NSInteger count = 0;
+//    count ++;
+//    NSInteger step = 1;
+//    if(myNum>3){
+//        step = -step;
+//    }
+////    mstr = [NSMutableString stringWithFormat:@"my-%zd",myNum++];
+//    indexPath = [NSIndexPath indexPathForRow:myNum+=step inSection:0];
+//    static NSIndexPath *staticPath;
+//    NSIndexPath *newPath = [[NSIndexPath indexPathForRow:myNum inSection:0] copy];
+//    staticPath = [newPath copy];
+//    NSLog(@"%zd--myNum:%zd",self.testDict[indexPath].boolValue,myNum);
+//    if(count<10){
+//    self.testDict[indexPath] = [NSNumber numberWithInteger:myNum];
+//    }
+//    else{
+//        self.testDict[newPath] = [NSNumber numberWithInteger:myNum];
+//    }
+//    
+//    NSLog(@"%@\n>>normal%@\n>>new:%@\n>>static:%@",self.testDict,indexPath,newPath,staticPath);
 }
 @end
--- MVVM_Demo/MVVM_Demo/LXGHomeViewController.m
+++ MVVM_Demo/MVVM_Demo/LXGHomeViewController.m
@@ -8,20 +8,125 @@
 
 #import "LXGHomeViewController.h"
 
+@interface LXGHomeViewController()<UITextFieldDelegate>
+
+{
+    UITextField *textField;
+}
+@end
 @implementation LXGHomeViewController
 -(void)viewDidLoad{
     [super viewDidLoad];
+    self.view.backgroundColor = [UIColor whiteColor];
     
-    RACSignal *signal = [RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
-        [subscriber sendNext:@1];
-        
-        return nil;
-        
-    }];
+//    textField = [[UITextField alloc]init];
+//    textField.frame = CGRectMake(10, 100, 300, 60);
+//    textField.backgroundColor = [UIColor grayColor];
+//    textField.delegate = self;
+//    [self.view addSubview:textField];
+//    
+//    [textField addTarget:self action:@selector(verifyText:) forControlEvents:UIControlEventEditingChanged];
     
-    [signal subscribeNext:^(id x) {
-        NSLog(@"%@",x);
-    }];
+    self.view.frame = CGRectMake(0,0, 414, 736);
+    [self.view addObserver:self forKeyPath:@"frame" options:NSKeyValueObservingOptionNew | NSKeyValueObservingOptionOld | NSKeyValueObservingOptionInitial context:@"xxx"];
+    self.view.backgroundColor = [UIColor redColor];
+//    UITableView *tb = [[UITableView alloc]init];
+//    tb.frame = self.view.bounds;
+//    [self.view addSubview:tb];
     
 }
+//-(void)loadViewIfRequired{
+//    
+//    NSLog(@"i'm coming");
+//}
+-(void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSString *,id> *)change context:(void *)context{
+    NSLog(@"%@",change);
+    NSLog(@"jskdjdl");
+}
+
+-(void)setView:(UIView *)view{
+    [super setView:view];
+//    self.view.frame = CGRectMake(200, 200, 200, 200);
+ 
+}
+
+//-(void)loadView{
+//    [super loadView];
+////    self.view.frame = CGRectMake(200, 200, 200, 200);
+// 
+//}
+
+
+//-(void)viewDidLayoutSubviews{
+////       self.view.frame = CGRectMake(200, 200, 200, 200); 
+//}
+
+-(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    
+    static NSInteger kkk = 1;
+    kkk = -kkk;
+    if(kkk>0){
+    [textField removeTarget:nil action:nil forControlEvents:UIControlEventEditingChanged];
+        [textField addTarget:self action:@selector(say:) forControlEvents:UIControlEventEditingChanged];
+    }
+    else{
+    [textField removeTarget:nil action:nil forControlEvents:UIControlEventEditingChanged];
+    [textField addTarget:self action:@selector(bark:) forControlEvents:UIControlEventEditingChanged];
+    }
+
+
+}
+
+-(void)say:(UITextField *)textfieldlocal{
+    NSLog(@"hello");
+}
+- (void)bark:(UITextField *)textfieldlocal{
+    NSLog(@"bark。。。");
+}
+-(void)verifyText:(UITextField *)textFieldlocal{
+    
+    NSLog(@"%@",textFieldlocal.text);
+    NSLog(@"%zd",[self caculateCountWithText:textFieldlocal.text]);
+}
+
+- (NSInteger)caculateCountWithText:(NSString *)text
+{
+    NSInteger length = 0;
+    
+    char *p = (char *)[text cStringUsingEncoding:NSUnicodeStringEncoding];
+    
+    for (NSInteger i = 0; i < [text lengthOfBytesUsingEncoding:NSUnicodeStringEncoding]; i++) {
+        if (*p) {
+            p++;
+            length++;
+        }
+        else {
+            p++;
+        }
+        
+    }
+    return length;
+}
+
+- (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string{
+    
+//    if((textField.text.length > 5) && ![string isEqualToString:@""]){
+//        return  NO;
+//    }
+    
+    return YES;
+
+}
+
+#pragma mark - UITableViewDataSource
+
+//- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+//    return 20;
+//}
+//
+//- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+//    UITableViewCell *cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"hhh"];
+//    return cell;
+//}
+
 @end
