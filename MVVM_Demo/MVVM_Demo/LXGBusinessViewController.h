b288994fd2135fda7de4151607ca1662d8c136d0
lixiaoguang <lixiaoguang@xiaoniu66.com>
2016-11-02 21:16:02 +0800

    add

--- MVVM_Demo/MVVM_Demo/LXGBusinessViewController.h
+++ MVVM_Demo/MVVM_Demo/LXGBusinessViewController.h
@@ -9,5 +9,6 @@
 #import <UIKit/UIKit.h>
 
 @interface LXGBusinessViewController : UIViewController
-
+/** name */
+@property (nonatomic, strong) NSString *name;
 @end
---
+++ MVVM_Demo/MVVM_Demo/tradeProtocol.h
@@ -0,0 +1,13 @@
+//
+//  tradeProtocol.h
+//  MVVM_Demo
+//
+//  Created by xiaoniu on 11/2/16.
+//  Copyright © 2016 xiaoniu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol tradeProtocol <NSObject>
+
+@end
