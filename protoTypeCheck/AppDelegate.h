05cc45a1e5869a06dfc299813b88f16ef7d3061e
lixiaoguang <lixiaoguang@xiaoniu66.com>
2016-12-14 17:19:22 +0800

    Initial Commit

---
+++ protoTypeCheck/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  protoTypeCheck
+//
+//  Created by xiaoniu on 12/14/16.
+//  Copyright © 2016 xiaoniu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ protoTypeCheck/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  protoTypeCheck
+//
+//  Created by xiaoniu on 12/14/16.
+//  Copyright © 2016 xiaoniu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
